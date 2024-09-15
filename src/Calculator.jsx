ced1bcbcd55ab25ccdfbad3ba561522989bbb941
Elizabeth Sotomayor <elizabethrsotomayor@gmail.com>
2024-09-14 20:24:30 -0500

    remove png icon from amount text field

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -12,22 +12,25 @@ function Calculator() {
 
         <form action="" method="get" className="mortgage-form">
           <div className="form-section">
+            {/* <label htmlFor="mortgage-amount" className="form-labels">Mortgage Amount</label>
+            <input type="text" name="mortgage-amount" id="mortgage-amount" className="text-fields text-field-amt" required /> */}
+
             <label htmlFor="mortgage-amount" className="form-labels">Mortgage Amount</label>
-            <input type="text" name="mortgage-amount" id="mortgage-amount" className="text-fields text-field-amt" required />
+            <label data-domain="$" className="static-overlay-labels mortgage-amount-label">
+            <input type="text" className="text-fields text-field-amount" id="mortgage-amount" required/>
+            </label>
           </div>
 
           <div className="term-rate-container">
             <div className="form-section">
               <label htmlFor="mortgage-term" className="form-labels">Mortgage Term</label>
-              <label data-domain="years" className="term-rate-label">
+              <label data-domain="years" className="static-overlay-labels term-rate-label">
               <input type="text" className="text-fields text-field-term" id="mortgage-term" required/>
               </label>
             </div>
             <div className="form-section">
-              {/* <label htmlFor="interest-rate" className="form-labels">Interest Rate</label>
-              <input type="text" name="interest-rate" id="interest-rate" className="text-fields" required /> */}
               <label htmlFor="mortgage-rate" className="form-labels">Interest Rate</label>
-              <label data-domain="%" className="term-rate-label">
+              <label data-domain="%" className="static-overlay-labels term-rate-label">
               <input type="text" className="text-fields text-field-term text-field-rate" id="interest-rate" required/>
               </label>
             </div>
@@ -36,8 +39,14 @@ function Calculator() {
           <div className="form-section">
             <fieldset className='mortgage-type-radio-btns'>
               <legend className="form-labels">Mortgage Type</legend>
-              <label><input type="radio" name="radio" value="yes" /> Repayment</label>
-              <label><input type="radio" name="radio" value="no" /> Interest Only</label>
+              <div className="radio-btns">
+                <input type="radio" name="mortgage-type" value="repayment" />
+                <label htmlFor='repayment' className="mortgage-type-label"> Repayment</label>
+              </div>
+              <div className="radio-btns">
+                <input type="radio" name="mortgage-type" value="interest-only" />
+                <label htmlFor='interest-only' className='mortgage-type-label'> Interest Only</label>
+              </div>
             </fieldset>
           </div>
 
