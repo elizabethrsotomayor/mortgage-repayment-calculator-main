b63d3cc92ebd3cafc66cd3c93466c49470a7ed06
Elizabeth Sotomayor <elizabethrsotomayor@gmail.com>
2024-09-19 13:17:30 -0500

    add hover states

--- src/Results.jsx
+++ src/Results.jsx
@@ -3,12 +3,12 @@ import './Results.css'
 function Results() {
   return (
     <>
-      {/* <div className="results results-empty">
+      <div className="results results-empty">
         <img src="../assets/images/illustration-empty.svg" alt="Results empty image" className='results-empty-img'/>
         <h2 className='results-heading'>Results shown here</h2>
         <p className='results-paragraph'>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
-      </div> */}
-      <div className='results results-populated'>
+      </div>
+      {/* <div className='results results-populated'>
         <h2 className='results-heading'>Your results</h2>
         <p className='results-paragraph results-paragraph-populated'>Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again.</p>
         <div className="results-display">
@@ -18,7 +18,7 @@ function Results() {
           <span className='results-paragraph'>Total you'll repay over the term</span>
           <h4 className='results-total'>$539,322.94</h4>
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
