02788b9e38956a4dfb883d2e70ded03fdf5f1843
Elizabeth Sotomayor <elizabethrsotomayor@gmail.com>
2024-09-26 15:33:21 -0500

    add react-hook-form

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -7,7 +7,8 @@ function Calculator({submitted, setSubmit, mortgageAmt, setMortgageAmt, term, se
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+    reset
+  } = useForm();  
 
   // Handle submission of calculator form
   const submitForm = (event) => {    
@@ -51,23 +52,44 @@ function Calculator({submitted, setSubmit, mortgageAmt, setMortgageAmt, term, se
           <div className="form-section">
             <label htmlFor="mortgage-amount" className="form-labels">Mortgage Amount</label>
             <label data-domain="$" className="static-overlay-labels mortgage-amount-label">
-            <input type="number" className="text-fields text-field-amount" id="mortgage-amount" value={mortgageAmt} onChange={(e) => setMortgageAmt(e.target.value)} required/>
+            <input 
+              type="number" 
+              className="text-fields text-field-amount" 
+              id="mortgage-amount" 
+              value={mortgageAmt} 
+              onChange={(e) => setMortgageAmt(e.target.value)} 
+              required
+              {...register('amount', { required: true, min: 1000, max: 2000000 })} />
             <span></span>
-            </label>
+            </label>            
           </div>
 
           <div className="term-rate-container">
             <div className="form-section">
               <label htmlFor="mortgage-term" className="form-labels">Mortgage Term</label>
               <label data-domain="years" className="static-overlay-labels term-rate-label">
-              <input type="number" className="text-fields text-field-term" id="mortgage-term" value={term} onChange={(e) => setTerm(e.target.value)} required/>
+              <input 
+                type="number" 
+                className="text-fields text-field-term" 
+                id="mortgage-term" 
+                value={term} 
+                onChange={(e) => setTerm(e.target.value)} 
+                required
+                {...register('term', { required: true, min: 5, max: 40 })}/>
               <span></span>
               </label>
             </div>
             <div className="form-section">
               <label htmlFor="interest-rate" className="form-labels">Interest Rate</label>
               <label data-domain="%" className="static-overlay-labels term-rate-label rate-label">
-              <input type="number" className="text-fields text-field-term text-field-rate" id="interest-rate" value={rate} onChange={(e) => setRate(e.target.value)} required/>
+              <input 
+                type="number" 
+                className="text-fields text-field-term text-field-rate" 
+                id="interest-rate" 
+                value={rate} 
+                onChange={(e) => setRate(e.target.value)} 
+                required
+                {...register('interest', { required: true, min: 1, max: 11 })}/>
               <span></span>
               </label>
             </div>
@@ -77,15 +99,34 @@ function Calculator({submitted, setSubmit, mortgageAmt, setMortgageAmt, term, se
             <fieldset className='mortgage-type-radio-btns'>
               <legend className="form-labels">Mortgage Type</legend>
               <div className={type === "repayment" ? "radio-btns radio-btn-active" : "radio-btns"}>
-                <input type="radio" name="mortgage-type" value="repayment" className={type === "repayment" ? "active-radio" : ""} onChange={(e) => setMortgageType(e.target.value)} checked={type === "repayment"} id="repayment" required/>
+                <input 
+                  type="radio" 
+                  name="mortgage-type" 
+                  value="repayment" 
+                  className={type === "repayment" ? "active-radio" : ""} 
+                  onChange={(e) => setMortgageType(e.target.value)} 
+                  checked={type === "repayment"} 
+                  id="repayment" 
+                  {...register('mortgagetype', { required: true })} />
                 <label htmlFor='repayment' className="mortgage-type-label"> Repayment</label>
               </div>
               <div className={type === "interest-only" ? "radio-btns radio-btn-active" : "radio-btns"}>
-                <input type="radio" name="mortgage-type" value="interest-only" className={type === "interest-only" ? "active-radio" : ""} onChange={(e) => setMortgageType(e.target.value)} checked={type === "interest-only"} id="interest-only"/>
-                {/* <span></span> */}
+                <input 
+                  type="radio" 
+                  name="mortgage-type" 
+                  value="interest-only" 
+                  className={type === "interest-only" ? "active-radio" : ""} 
+                  onChange={(e) => setMortgageType(e.target.value)} 
+                  checked={type === "interest-only"} 
+                  id="interest-only"
+                  {...register('mortgagetype', { required: true })}/>
                 <label htmlFor='interest-only' className='mortgage-type-label'> Interest Only</label>
               </div>
             </fieldset>
+            {
+              errors.mortgagetype && <p className='error_p'>This field is required</p>
+            }
+           {console.log(errors.mortgagetype)}
           </div>
 
           <div className="form-section">
